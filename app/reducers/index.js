79b7462f2da360ed52c132751b5f19cc2ef03f7f
Rommel Gallofin <rommelngallofin@su.edu.ph>
2018-05-15 13:37:07 +0800

    index

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,4 +1,12 @@
 import { combineReducers } from 'redux';
 import * as recipesReducer from './recipes';
 
-export default combineReducers(Object.assign(recipesReducer));
+//export default combineReducers(Object.assign(recipesReducer));
+
+export default rootReducer = (state, action) => {
+    //state = undefined
+    return appReducer(state, action)
+  }
+
+const appReducer = combineReducers(Object.assign(recipesReducer))
+  
